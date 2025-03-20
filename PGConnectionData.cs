149f7f1c6cb486b3d4a129a60c9e530f61ddc394
Christoph Bornhardt <cbornhardt@outlook.com>
2025-03-20 15:53:59 -0300

    Update PGConnectionData.cs

--- PGConnectionData.cs
+++ PGConnectionData.cs
@@ -1,15 +1,25 @@
 ﻿namespace DBShake;
 
-public sealed class PGConnectionData :IDBConnectionData
+public sealed class PGConnectionData : IDBConnectionData
 {
     public const ConnectionTypeName TypeName = ConnectionTypeName.PG;
     public string ConnectionType { get; set; }
 
     public string Server { get; set; }
-    public int Port { get; set; }
+    public int? Port { get; set; }
     public string Database { get; set; }
     public string UserId { get; set; }
     public string Password { get; set; }
 
-    public string ConnectionString => $"Server={Server};Port={Port};Database={Database};User Id={UserId};Password={Password};";
+    public string ConnectionString
+    {
+        get
+        {
+            string port = "";
+            if (Port is not null)
+                port = $"Port={Port};";
+
+            return $"Server={Server};{port}Database={Database};User Id={UserId};Password={Password};";
+        }
+    }
 }
